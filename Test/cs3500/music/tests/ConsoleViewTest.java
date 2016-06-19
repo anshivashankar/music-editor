a45dfccd56c22bc3b6146419f23026888dd2c7b8
Ashwin ShivaShankar <shivashankar.a@husky.neu.edu>
2016-06-18 21:03:48 -0400

    Ashwin 6/18/16
    Finished tests for ConsoleView.

--- Test/cs3500/music/tests/ConsoleViewTest.java
+++ Test/cs3500/music/tests/ConsoleViewTest.java
@@ -112,6 +112,32 @@ public class ConsoleViewTest {
                     "10  |    |         |  \n", build.toString());
   }
 
+  @Test
+  public void testViewChordFatter() throws Exception {
+    Appendable build = new StringBuilder();
+    IMusicModel<Note> piece = new Piece();
+    piece.addAll(new ArrayList<Note>(
+            Arrays.asList(new Note(Note.Pitch.D_SHARP, Note.Octave.Four, 4, 7),
+                    new Note(Note.Pitch.C_SHARP, Note.Octave.Five, 4, 7),
+                    new Note(Note.Pitch.C, Note.Octave.Five, 4, 7))));
+    MusicControllerImpl<Note> controller = new MusicControllerImpl<Note>(piece);
+    IView console = new ConsoleView(build, controller);
+    console.view();
+    assertEquals(
+                    "   D#4   E4   F4  F#4   G4  G#4   A4  A#4   B4   C5  C#5 \n" +
+                    " 0                                                       \n" +
+                    " 1                                                       \n" +
+                    " 2                                                       \n" +
+                    " 3                                                       \n" +
+                    " 4                                                       \n" +
+                    " 5                                                       \n" +
+                    " 6                                                       \n" +
+                    " 7  X                                            X    X  \n" +
+                    " 8  |                                            |    |  \n" +
+                    " 9  |                                            |    |  \n" +
+                    "10  |                                            |    |  \n", build.toString());
+  }
+
 
   @Test
   public void testEmptyView() {
@@ -123,51 +149,4 @@ public class ConsoleViewTest {
     assertEquals("", build.toString());
   }
 
-/*
-  @Test
-  public void testShowPieceNormal() {
-    IMusicModel<Note> m1 = new Piece();
-    m1.addAll(new ArrayList<Note>(
-            Arrays.asList(new Note(Note.Pitch.D_SHARP, Note.Octave.Five, 4, 3),
-                    new Note(Note.Pitch.D, Note.Octave.Five, 2, 0),
-                    new Note(Note.Pitch.A, Note.Octave.Five, 2, 0))));
-
-    assertEquals(
-            "   D5  D#5   E5   F5  F#5   G5  G#5   A5 \n" +
-                    "0  X                                  X  \n" +
-                    "1  |                                  |  \n" +
-                    "2                                        \n" +
-                    "3       X                                \n" +
-                    "4       |                                \n" +
-                    "5       |                                \n" +
-                    "6       |                                \n", m1.showPiece());
-  }
-
-  @Test
-  public void testShowPieceOverLap() {
-    IMusicModel<Note> m1 = new Piece();
-    m1.addAll(new ArrayList<Note>(
-            Arrays.asList(new Note(Note.Pitch.D_SHARP, Note.Octave.Five, 4, 3),
-                    new Note(Note.Pitch.D, Note.Octave.Five, 2, 0),
-                    new Note(Note.Pitch.A, Note.Octave.Five, 2, 0),
-                    new Note(Note.Pitch.D_SHARP, Note.Octave.Five, 2, 5))));
-
-    assertEquals(
-            "   D5  D#5   E5   F5  F#5   G5  G#5   A5 \n" +
-                    "0  X                                  X  \n" +
-                    "1  |                                  |  \n" +
-                    "2                                        \n" +
-                    "3       X                                \n" +
-                    "4       |                                \n" +
-                    "5       X                                \n" +
-                    "6       |                                \n", m1.showPiece());
-  }
-
-  @Test
-  public void testEmptyPiece() {
-    IMusicModel<Note> m1 = new Piece();
-    assertEquals(
-            "", m1.showPiece());
-  }
-*/
 }
