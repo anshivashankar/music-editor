da1e0b2ed1237019ad2212b14178f856bcb588cc
Ashwin ShivaShankar <shivashankar.a@husky.neu.edu>
2016-06-23 23:33:58 -0400

    Ashwin 6/18/16
    Added CombinedView

---
+++ src/cs3500/music/view/CombinedView.java
@@ -0,0 +1,28 @@
+package cs3500.music.view;
+
+/**
+ * Created by ashwinshivashankar on 6/23/16.
+ */
+public class CombinedView implements View {
+
+  long time;
+
+  public CombinedView() {
+    this.time = 0;
+  }
+
+  @Override
+  public void view() {
+
+  }
+
+  @Override
+  public void play() {
+
+  }
+
+  @Override
+  public void pause() {
+
+  }
+}
